10e05c809f9557d852027dabde23c6137dc37713
Anitha-najani <anithanajani26@gmail.com>
2022-11-30 16:51:33 +0530

    learned about props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <section>
+        <PropsExample name="Anitha" age="20"/>
+        <PropsExample name="Raji" age="26"/>
+        {/*<p>This is App</p>
+        <h1>Heading Tag</h1>
+        <img src="https://images.firstpost.com/wp-content/uploads/2022/02/tom-and-jerry.jpeg?impolicy=website&width=700&height=600" alt="tom and jerry"/>
+        <ClassComponent/>*/}
+    </section>
+    )
 }
-
-export default App;
+export default App
---
+++ src/ClassComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+class ClassComponent extends React.Component{
+	render(){
+		return(
+			<div>
+				This is class component
+			</div>
+			)
+	}
+}
+export default ClassComponent
